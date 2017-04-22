3202ac2717c6663dff16dc1573f81dcb876e3e4b
naoto0822 <n.h.in.m.h@gmail.com>
2017-04-22 13:37:06 +0900

    add pub_date accessor in article

--- src/app/article.rb
+++ src/app/article.rb
@@ -1,12 +1,12 @@
 module R2S
   class Article
-    attr_reader :id, :title, :desc, :body, :url, :created_at
-    def initialize(id:nil, title:nil, desc:nil, body:nil, url:nil, created_at:nil)
+    attr_reader :id, :title, :body, :url, :pub_date, :created_at
+    def initialize(id:nil, title:nil, body:nil, url:nil, pub_date:nil, created_at:nil)
       @id = id
       @title = title
-      @desc = desc
       @body = body
       @url = url
+      @pub_date = pub_date
       @created_at = created_at
     end
   end
