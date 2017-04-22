9f5c693aa4a92ec952f0c15ab58cacbb33bf1e27
naoto0822 <n.h.in.m.h@gmail.com>
2017-04-22 14:17:16 +0900

    rename rss fetcher

---
+++ src/app/rss_fetcher.rb
@@ -0,0 +1,60 @@
+require 'rss'
+
+module R2S
+  class RSSFetcher
+    def initialize(logger:logger)
+      @logger = logger
+      @rss = nil
+    end
+
+    def fetch(url)
+      return nil if url.nil? || url.empty?
+      begin
+        @logger.debug("start fetch rss feed from #{url}")
+        @rss = request(url)
+      rescue => e
+        @logger.warn("fail rss feed from #{url}, #{e.message}")
+        return nil
+      end
+
+      return nil if @rss.nil?
+      begin
+        results = parse(@rss)
+        @logger.debug("success fetch rss feed from #{url}")
+        return results
+      rescue => e
+        @logger.warn("fail parsing feed item", e.message)
+        return nil
+      end
+    end
+
+    private
+
+    def request(url)
+      begin
+        RSS::Parser.parse(url)
+      rescue RSS::InvalidRSSError
+        RSS::Parser.parse(url, false)
+      rescue => e
+        raise e
+      end
+    end
+
+    def parse(rss)
+      article_arr = []
+      begin
+        rss.items.each do |item|
+          title = item.title
+          body = item.description
+          url = item.link
+          pub_date = item.pubDate
+          article = R2S::Article.new(title:title, body:body, url:url, pub_date:pub_date)
+          article_arr.push article
+        end
+      rescue => e
+        return e
+      end
+      article_arr
+    end
+  end
+end
