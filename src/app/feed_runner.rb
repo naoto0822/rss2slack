5a33ed8cf8b1645df87ea40d9d8e3a0c2471902f
naoto0822 <n.h.in.m.h@gmail.com>
2017-04-23 16:05:25 +0900

    fix runner

--- src/app/feed_runner.rb
+++ src/app/feed_runner.rb
@@ -1,26 +1,43 @@
 require_relative './db_client'
 require_relative './feed_model'
 require_relative './feed.rb'
-require_relative './rss_client'
+require_relative './article_model'
+require_relative './article'
+require_relative './rss_fetcher'
 
 module R2S
-  class Runner
-    def initalize(logger:logger, conf:conf)
+  class FeedRunner
+    def initalize(logger: logger, conf: conf)
       @logger = logger
       @conf = conf
-      @db = R2S::DBClient.new(logger:logger, conf:conf)
-      @feed_model = R2S::FeedModel.new(logger:logger, conf:conf, db:@db)
-      @rss = R2S::RssClient.new(logger:logger)
+      @db = R2S::DBClient.new(logger: logger, conf: conf)
+      @feed_model = R2S::FeedModel.new(logger: logger, db: @db)
+      @article_model = R2S::ArticleModel.new(logger: logger, db: @db)
+      @rss = R2S::RSSFetcher.new(logger: logger)
     end
 
     def run
-      articles = []
+      @logger.debug('start running FeedRunner.')
       feeds = @feed_model.find_all
       feeds.each do |f|
-
+        next if f.url.nil?
+        articles = @rss.fetch(f.url)
+        articles.each { |a| @article_model.save(a) } #TODO: bulk insert
       end
 
+      # fetch today article.
+
       # post slack
+      
+      @logger.debug('finish running FeedRunner.')
+    end
+
+    def fetch_articles
+
+    end
+
+    def post_slack
+
     end
   end
 end
