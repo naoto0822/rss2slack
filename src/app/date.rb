8ca04a1eabf6edc51445f1b8371bb2ec4e90e60e
naoto0822 <n.h.in.m.h@gmail.com>
2017-04-28 01:28:54 +0900

    create date class

---
+++ src/app/date.rb
@@ -0,0 +1,13 @@
+module R2S
+  class Date
+    def self.now
+      Time.now
+    end
+    def self.yesterday(date=Time.now)
+      date - (24 * 60 * 60)
+    end
+    def self.format(date)
+      date.strftime('%Y-%m-%d %H:%M:%S')
+    end
+  end
+end
