fbffba17de21b0d366f404ac0659e24204eea184
naoto0822 <n.h.in.m.h@gmail.com>
2017-04-30 00:08:24 +0900

    fix comment out

--- src/app/slack_msg_builder.rb
+++ src/app/slack_msg_builder.rb
@@ -2,11 +2,17 @@ require_relative './slack/webhooks.rb'
 
 module R2S
   class SlackMsgBuilder
+    # for feed_runner
     def self.build_for_feed
       payload = Slack::IncomingWebhooks::Payload.new
       payload.username = 'Rss2Slack'
       payload.text = 'Successfully fetched feed :tada: '
       payload
+    end
+
+    # for article_runner
+    def self.build_for_article(articles)
+      # TODO
     end
   end
 end
