68f7ff2c7ba23ff5dbad6191e21b184edc4f9751
naoto0822 <n.h.in.m.h@gmail.com>
2017-04-30 16:25:19 +0900

    update rubocop

--- src/app/test.rb
+++ src/app/test.rb
@@ -2,23 +2,23 @@
 require_relative './slack/webhooks'
 
 field1 = Slack::IncomingWebhooks::Field.new
-field1.title = "Article1"
-field1.value = "https://google.com"
+field1.title = 'Article1'
+field1.value = 'https://google.com'
 field1.short = false
 field2 = Slack::IncomingWebhooks::Field.new
-field2.value = "http://yahoo.co.jp"
+field2.value = 'http://yahoo.co.jp'
 field2.short = false
 
 attachment = Slack::IncomingWebhooks::Attachment.new
-attachment.fallback = "Required plain-text summary of the attachment."
-attachment.color = "good"
-attachment.title = "8月22日の記事"
+attachment.fallback = 'Required plain-text summary of the attachment.'
+attachment.color = 'good'
+attachment.title = '8月22日の記事'
 attachment.fields = [field1, field2]
-attachment.footer = "from Rss2Slack"
+attachment.footer = 'from Rss2Slack'
 attachment.ts = 123456789
 
 payload = Slack::IncomingWebhooks::Payload.new
-payload.username = "Rss2Slack"
+payload.username = 'Rss2Slack'
 payload.text = "Today's Feed :tada:"
 payload.attachments = [attachment]
 
