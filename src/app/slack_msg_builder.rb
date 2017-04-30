28e7b67c53e3e7a87aa5cebb43a3ac0fecb79359
naoto0822 <n.h.in.m.h@gmail.com>
2017-04-30 13:23:28 +0900

    payload builder for article runner

--- src/app/slack_msg_builder.rb
+++ src/app/slack_msg_builder.rb
@@ -1,4 +1,5 @@
 require_relative './slack/webhooks.rb'
+require_relative './date'
 
 module R2S
   class SlackMsgBuilder
@@ -12,7 +13,30 @@ module R2S
 
     # for article_runner
     def self.build_for_article(articles)
-      # TODO
+      payload = Slack::IncomingWebhooks::Payload.new
+      attachment = Slack::IncomingWebhooks::Attachment.new
+      now = R2S::Date::now
+
+      attachment.fallback = "Today's Feed"
+      attachment.color = 'good'
+      attachment.title = "#{now.month}月#{now.day}日の記事 :tada: "
+      attachment.footer = 'from Rss2Slack'
+      attachment.ts = now.to_i
+
+      fields = []
+      articles.each { |a|
+        field = Slack::IncomingWebhooks::Field.new
+        field.title = a.title
+        field.value = a.url
+        field.short = false
+        fields.push(field)
+      }
+      attachment.fields = fields
+
+      payload.attachments = [attachment]
+      payload.username = 'Rss2Slack'
+      payload.text = "Today's Feed :tada: "
+      payload
     end
   end
 end
