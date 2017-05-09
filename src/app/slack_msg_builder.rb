ce9e4d5f8616045a850fc71099459fa5544290e8
naoto0822 <n.h.in.m.h@gmail.com>
2017-05-10 00:10:06 +0900

    modify body builder

--- src/app/slack_msg_builder.rb
+++ src/app/slack_msg_builder.rb
@@ -4,6 +4,7 @@ require_relative './date'
 module R2S
   class SlackMsgBuilder
     # for feed_runner
+    # TODO: rename
     def self.build_for_feed
       payload = Slack::IncomingWebhooks::Payload.new
       payload.username = 'Rss2Slack'
@@ -12,6 +13,7 @@ module R2S
     end
 
     # for article_runner
+    # TODO: rename
     def self.build_for_article(articles)
       payload = Slack::IncomingWebhooks::Payload.new
       attachment = Slack::IncomingWebhooks::Attachment.new
@@ -36,6 +38,30 @@ module R2S
       payload.attachments = [attachment]
       payload.username = 'Rss2Slack'
       payload.text = "Today's Feed :tada: "
+      payload
+    end
+
+    # invalid: POST v1/slack/feed
+    def self.build_for_invalid_feed(title, url)
+      payload = Slack::IncomingWebhooks::Payload.new
+      attachment = Slack::IncomingWebhooks::Attachment.new
+      now = R2S::Date::now
+
+      attachment.fallback = "Failure register Feed: #{url}"
+      attachment.color = 'danger'
+      attachment.title = '記事の登録が失敗しました'
+      attachment.footer = 'from Rss2Slack'
+      attachment.ts = now.to_i
+
+      field = Slack::IncomingWebhooks::Field.new
+      field.title = title
+      field.value = url
+      field.short = false
+
+      attachment.fields = [field]
+      payload.attachments = [attachment]
+      payload.username = 'Rss2Slack'
+      payload.title = 'Failure :tada:'
       payload
     end
   end
