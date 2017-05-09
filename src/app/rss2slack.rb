5d4085d188c1569ee1062adb2786a0f2feb03b06
naoto0822 <n.h.in.m.h@gmail.com>
2017-05-10 00:13:07 +0900

    handle response

--- src/app/rss2slack.rb
+++ src/app/rss2slack.rb
@@ -13,4 +13,10 @@ class Rss2Slack < Sinatra::Base
     p 'params'
     p request.params
   end
+
+  def handle_response(response)
+    status response.status unless response.status.nil?
+    headers response.headers unless response.headers.nil? && response.headers.empty?
+    body response.body unless response.body.nil?
+  end
 end
