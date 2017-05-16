4567a050b13138baff686167197d0ef0512b205f
naoto0822 <n.h.in.m.h@gmail.com>
2017-05-16 22:40:11 +0900

    refactor response class

--- src/app/response.rb
+++ src/app/response.rb
@@ -1,10 +1,22 @@
 module R2S
   class Response
+    OK_CODE = 200.freeze
+    BAD_REQUEST_CODE = 400.freeze
+
     attr_reader :code, :headers, :body
+
     def initialize(code, headers, body)
       @code = code
       @headers = headers
       @body = body
+    end
+
+    def self.create_ok(headers, body)
+      Response.new(OK_CODE, headers, body)
+    end
+
+    def self.create_bad_request(headers, body)
+      Response.new(BAD_REQUEST_CODE, headers, body)
     end
   end
 end
