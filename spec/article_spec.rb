82112b2f8924f3eed2624df9ae6a187f89d9a132
naoto0822 <n.h.in.m.h@gmail.com>
2017-05-21 13:22:58 +0900

    fix typo

--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -12,13 +12,13 @@ describe R2S::Article do
 
   it 'article initialize' do
     article = R2S::Article.new(id:1, title:'hoge', body:'<p>hoge</p>',
-                               url: 'http://yahoo.co.jp', pub_date:'1990', create_at:'1990-08-22')
+                               url: 'http://yahoo.co.jp', pub_date:'1990', created_at:'1990-08-22')
     expect(article.id).to eq 1
     expect(article.title).to eq 'hoge'
     expect(article.body).to eq '<p>hoge</p>'
     expect(article.url).to eq 'http://yahoo.co.jp'
     expect(article.pub_date).to eq '1990'
-    expect(article.create_at).to eq '1990-08-22'
+    expect(article.created_at).to eq '1990-08-22'
   end
 
   it 'no set args' do
@@ -28,6 +28,6 @@ describe R2S::Article do
     expect(article.body).to eq nil
     expect(article.url).to eq nil
     expect(article.pub_date).to eq nil
-    expect(article.create_at).to eq nil
+    expect(article.created_at).to eq nil
   end
 end
