3618841192677aceaaf024d09cefe12ea7b55b76
naoto0822 <n.h.in.m.h@gmail.com>
2017-05-24 21:50:23 +0900

    refactor handler

--- src/app/rss2slack_handler.rb
+++ src/app/rss2slack_handler.rb
@@ -7,6 +7,11 @@ require_relative './slack_msg_builder'
 
 module R2S
   class Handler
+    REGISTER_COMMAND = 'rss2slack_register'.freeze
+    UPDATE_COMMAND = 'rss2slack_update'.freeze
+    DELETE_COMMAND = 'rss2slack_delete'.freeze
+    HELP_COMMAND = 'rss2slack_help'.freeze
+
     def initialize(logger, conf, db)
       @logger = logger
       @conf = conf
@@ -16,56 +21,100 @@ module R2S
 
     def handle_slack_feed(headers, body)
       @logger.debug('start handle_slack_feed()')
+      if body.nil?
+        @logger.warn('request body is nil')
+        return bad_request_response
+      end
+
       data = Slack::OutgoingWebhooks::Message.new(body)
 
       unless valid_outgoing_webhook?(data)
-        msg = "invalid request: #{data}"
-        @logger.warn(msg)
-        return bad_request_response(msg)
+        @logger.warn("invalid request: #{data}")
+        return bad_request_response
       end
 
       if split_post_text(data.text).nil?
-        msg = "invalid text: #{data.text}"
-        @logger.warn(msg)
-        return bad_request_response(msg)
-      end
-      
-      title, url = split_post_text(data.text)
-      feeds = @feed_model.find_by_url(url)
-      if feeds.length >= 1
-        msg = "already registerd url: #{url}"
-        @logger.warn(msg)
-        return ok_response(msg)
+        @logger.warn("invalid text: #{data.text}")
+        return bad_request_response
       end
 
-      @feed_model.save(title, url)
-      @logger.debug("finish handle_slack_feed(), text: #{text}")
-      ok_response("successflluy registerd #{title}")
+      texts = split_post_text(data.text)
+
+      case texts[0]
+      when REGISTER_COMMAND
+        return handle_slack_register_feed(data)
+      when UPDATE_COMMAND
+        return handle_slack_update_feed(data)
+      when DELETE_COMMAND
+        return handle_slack_delete_feed(data)
+      when HELP_COMMAND
+        return handle_slack_help
+      else
+        return bad_request_response
+      end
     rescue => e
       raise e
     end
 
     private
 
+    def handle_slack_register_feed(data, texts)
+      @logger.debug('start handle_slack_register_feed()')
+      # FIXME: 3?
+      if texts.length < 3
+        @logger.warn('texts size shorter than 3')
+        return bad_request_response
+      end
+
+      title = texts[1]
+      url = texts[2]
+      feeds = @feed_model.find_by_url(url)
+      if feeds.length >= 1
+        return ok_response("already registerd url: #{url}")
+      end
+
+      @feed_model.save(title, url)
+      @logger.debug("finish handle_slack_register_feed(), text: #{data.text}")
+      ok_response("successflluy registerd #{title}")
+    end
+
+    # TODO:
+    def handle_slack_update_feed(data)
+      ok_response('Unimplemented rss2slack_update command')
+    end
+
+    # TODO:
+    def handle_slack_delete_feed(data)
+      ok_response('Unimplemented rss2slack_delete command')
+    end
+
+    # TODO:
+    def handle_slack_help
+      ok_response('Unimplemented rss2slack_help command')
+    end
+
     def ok_response(msg)
       body = R2S::SlackMsgBuilder.build_for_success(msg).to_params.to_json
       R2S::Response::create_ok(nil, body)
     end
 
-    def bad_request_response(msg)
+    def bad_request_response
+      # return empty body
       body = ''
       R2S::Response::create_bad_request(nil, body)
     end
 
-    # text: 'rss2slack_register, feed_name, url'
+    # example:
+    #   'rss2slack_register, feed_name, url'
+    #   'rss2slack_update, url'
+    #   'rss2slack_delete, url'
+    #   'rss2slack_help'
     def split_post_text(text)
       arr = text.split(/\s*,\s*/)
-      return nil if arr.nil? || arr.empty? || arr.size < 3
-      title = arr[1]
-      url = arr[2]
-      [title, url]
+      return nil if arr.nil? || arr.empty?
+      arr
     rescue
-      @logger.warn("failure split post text")
+      @logger.warn('failure split post text')
       nil
     end
 
