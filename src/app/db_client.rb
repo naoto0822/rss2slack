b482a4742096998a1589d2ae2e95000e6808ce61
naoto0822 <n.h.in.m.h@gmail.com>
2017-06-08 01:31:51 +0900

    adapter sqlite3 client

--- src/app/db_client.rb
+++ src/app/db_client.rb
@@ -1,26 +1,42 @@
 require 'mysql2'
+require 'sqlite3'
 
 module R2S
   class DBClient
-    attr_accessor :client, :logger, :conf
+    attr_accessor :db, :logger, :conf
     def initialize(logger, conf)
       @logger = logger
       @conf = conf
+      @db = client
+    end
 
-      @client = Mysql2::Client.new(host: conf.db_host,
-                                   database: conf.db_name,
-                                   username: conf.db_username,
-                                   password: conf.db_password)
+    def client
+      if @conf.dev?
+        return sqlite3_client
+      end
+      mysql_client
+    end
+
+    def mysql_client
+      Mysql2::Client.new(host: @conf.db_host,
+                         database: @conf.db_name,
+                         username: @conf.db_username,
+                         password: @conf.db_password)
+
+    end
+
+    def sqlite3_client
+      SQLite3::Database.new(@conf.db_name)
     end
 
     def statement(sql)
       @logger.debug("execute query: #{sql}")
-      @client.prepare(sql)
+      @db.prepare(sql)
     end
 
     def execute(sql)
       @logger.debug("execute query: #{sql}")
-      @client.query(sql)
+      @db.query(sql)
     end
   end
 end
