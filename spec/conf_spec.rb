94a55a5d8ec52a969f24141657f781164295483a
naoto0822 <n.h.in.m.h@gmail.com>
2017-06-12 23:42:21 +0900

    update conf test

--- spec/conf_spec.rb
+++ spec/conf_spec.rb
@@ -23,7 +23,18 @@ describe R2S::Conf do
     expect(conf.test?).to eq true
   end
 
-  it 'conf path' do
+  it 'accessor' do
     conf = R2S::Conf.new
+
+    expect(conf.webhook_url).to eq 'https://hooks.slack.com/'
+    expect(conf.db_host).to eq 'localhost'
+    expect(conf.db_name).to eq 'test'
+    expect(conf.db_username).to eq 'test_user'
+    expect(conf.db_password).to eq 'test_pass'
+    expect(conf.slack_token).to eq 'TEST_TOKEN'
+    expect(conf.accept_team_domain).to eq 'test.domain'
+    expect(conf.accept_channel_id).to eq 'channel_id'
+    expect(conf.logger_runner_path).to eq '/var/log/rss2slack/runner.log'
+    expect(conf.logger_app_path).to eq '/var/log/rss2slack/app.log'
   end
 end
