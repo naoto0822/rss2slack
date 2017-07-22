607e94467a64f3495584d66fe32096809b2cfb1a
naoto0822 <n.h.in.m.h@gmail.com>
2017-07-23 01:56:30 +0900

    create tmp root user for mysql

---
+++ scripts/create_mysql_root_user.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+systemctl start mysqld
+
+root_mycnf=$HOME/.my.cnf
+tmp_pass=$(cat /var/log/mysqld.log | grep "temporary password" | awk '{print $11}')
+cat > $root_mycnf <<EOF
+[client]
+user=root
+password=$tmp_pass
+EOF
