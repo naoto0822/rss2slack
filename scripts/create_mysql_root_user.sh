45df2a07ba351ce3918a78df53f9aac526ab8696
naoto0822 <n.h.in.m.h@gmail.com>
2017-07-25 01:16:27 +0900

    rename my.cnf for tmp root user

--- scripts/create_mysql_root_user.sh
+++ scripts/create_mysql_root_user.sh
@@ -2,7 +2,7 @@
 
 systemctl start mysqld
 
-root_mycnf=$HOME/.my.cnf
+root_mycnf=$HOME/.tmp.my.cnf
 tmp_pass=$(cat /var/log/mysqld.log | grep "temporary password" | awk '{print $11}')
 cat > $root_mycnf <<EOF
 [client]
