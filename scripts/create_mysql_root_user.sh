89553fa36098d7fa8ff37163b77abfa66476db5c
naoto0822 <n.h.in.m.h@gmail.com>
2017-07-26 02:04:04 +0900

    change permission my.cnf

--- scripts/create_mysql_root_user.sh
+++ scripts/create_mysql_root_user.sh
@@ -4,8 +4,9 @@ systemctl start mysqld
 
 root_mycnf=$HOME/.tmp.my.cnf
 tmp_pass=$(cat /var/log/mysqld.log | grep "temporary password" | awk '{print $11}')
+umask 0077
 cat > $root_mycnf <<EOF
 [client]
 user=root
-password=$tmp_pass
+password="$tmp_pass"
 EOF
