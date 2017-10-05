052e84ef5e4d0d7063eff90f64349c901aa32431
naoto0822 <n.h.in.m.h@gmail.com>
2017-10-05 23:25:25 +0900

    fix work dir path

--- playbook/roles/app/templates/dev_unicorn_conf.rb
+++ playbook/roles/app/templates/dev_unicorn_conf.rb
@@ -1,10 +1,9 @@
-# /var/www/lib
 @app_name = "rss2slack"
-@dir = "/var/www/#{@app_name}"
+@app_path = "/var/www/#{@app_name}"
 preload_app true
 
 worker_processes {{ dev_unicorn_worker_processes }} 
-working_directory @dir
+working_directory "#{@app_path}/current"
 
 timeout {{ unicorn_timeout }}
 listen "/tmp/unicorn.sock"
