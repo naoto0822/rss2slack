fb493b4000135b6019b709becdc9a2bd7ce2c89e
naoto0822 <n.h.in.m.h@gmail.com>
2017-10-11 02:20:13 +0900

    fix unicorn handle when before_fork

--- playbook/roles/app/templates/prod_unicorn_conf.rb
+++ playbook/roles/app/templates/prod_unicorn_conf.rb
@@ -11,3 +11,13 @@ pid "/tmp/unicorn.pid"
 
 stderr_path "/var/log/unicorn/#{@app_name}_stderr.log"
 stdout_path "/var/log/unicorn/#{@app_name}_stdout.log"
+
+before_fork do |server, worker|
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill('QUIT', File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
