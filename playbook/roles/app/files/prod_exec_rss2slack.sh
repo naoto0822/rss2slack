69a5d64bdc1ef360ca5364912a7a8cdecdba1166
naoto0822 <n.h.in.m.h@gmail.com>
2017-10-21 16:02:55 +0900

    create prod exec sh

---
+++ playbook/roles/app/files/prod_exec_rss2slack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PID_COUNT=$(ps aux | grep "unicorn master" | grep -v grep | wc -l)
+
+if [ $PID_COUNT -eq 0 ]; then
+  if [ -e /tmp/unicorn.pid ]; then
+    rm -f /tmp/unicorn.pid
+  fi
+  if [ -e /tmp/unicorn.sock ]; then
+    rm -f /tmp/unicorn.sock
+  fi
+  cd /var/www/rss2slack/current
+  /usr/local/rbenv/bin/rbenv exec bundle exec unicorn -c /etc/unicorn/unicorn_conf.rb -E production -D
+fi
