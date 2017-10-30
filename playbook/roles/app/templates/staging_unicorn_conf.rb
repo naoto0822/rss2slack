955d553da5041b7401615b7b936eabd55d983844
naoto0822 <n.h.in.m.h@gmail.com>
2017-10-30 20:21:10 +0900

    add staging conf

---
+++ playbook/roles/app/templates/staging_unicorn_conf.rb
@@ -0,0 +1,23 @@
+@app_name = "rss2slack"
+@app_path = "/var/www/#{@app_name}"
+preload_app true
+
+worker_processes {{ dev_unicorn_worker_processes }} 
+working_directory "#{@app_path}/current"
+
+timeout {{ unicorn_timeout }}
+listen "/tmp/unicorn.sock"
+pid "/tmp/unicorn.pid"
+
+stderr_path "/var/log/unicorn/#{@app_name}_stderr.log"
+stdout_path "/var/log/unicorn/#{@app_name}_stdout.log"
+
+before_fork do |server, worker|
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill('QUIT', File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
