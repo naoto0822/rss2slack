85eefe653cf02697dfdeefbfb3f55b651715bcd1
naoto0822 <n.h.in.m.h@gmail.com>
2017-10-30 20:07:32 +0900

    fix staging playbook option

--- playbook/staging_playbook.sh
+++ playbook/staging_playbook.sh
@@ -2,6 +2,6 @@
 
 PRIVATE_KEY=$(vagrant ssh-config | grep "IdentityFile" | grep "/staging/" | awk '{print $2}');
 ansible-playbook -i hosts \
-                 -l dev \
+                 -l staging \
                  --private-key $PRIVATE_KEY \
                  staging_server.yml;
