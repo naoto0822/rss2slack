81ebb41b0f651e303788e3b27b1dff47a4408b6e
naoto0822 <n.h.in.m.h@gmail.com>
2017-11-03 02:10:17 +0900

    fix app_path in dev unicorn conf

--- playbook/roles/app/templates/dev_unicorn_conf.rb
+++ playbook/roles/app/templates/dev_unicorn_conf.rb
@@ -1,9 +1,9 @@
 @app_name = "rss2slack"
-@app_path = "/var/www/#{@app_name}"
+@app_path = "/home/rss2slack/#{@app_name}"
 preload_app true
 
 worker_processes {{ dev_unicorn_worker_processes }} 
-working_directory "#{@app_path}/current"
+working_directory @app_path
 
 timeout {{ unicorn_timeout }}
 listen "/tmp/unicorn.sock"
