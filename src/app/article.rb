ab75ebc5ee1bca700b911ccedeb8261472592370
naoto0822 <n.h.in.m.h@gmail.com>
2017-11-03 15:29:25 +0900

    check publish date

--- src/app/article.rb
+++ src/app/article.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 module R2S
   class Article
     attr_reader :id, :title, :body, :url, :pub_date, :created_at
@@ -7,8 +9,18 @@ module R2S
       @title = title
       @body = body
       @url = url
-      @pub_date = pub_date
+      @pub_date = check_pub_date(pub_date)
       @created_at = created_at
+    end
+
+    private
+
+    def check_pub_date(date)
+      return nil if date.nil?
+      time = Time.parse(date)
+      time.strftime('%Y-%m-%d %H:%M:%S')
+    rescue
+      return nil
     end
   end
 end
