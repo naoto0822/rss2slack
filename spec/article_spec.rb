3045849982d2f9e9c374ec0ec064dd646a168a21
naoto0822 <n.h.in.m.h@gmail.com>
2017-11-06 00:51:30 +0900

    update spec

--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -11,13 +11,17 @@ describe R2S::Article do
   end
 
   it 'article initialize' do
-    article = R2S::Article.new(id:1, title:'hoge', body:'<p>hoge</p>',
-                               url: 'http://yahoo.co.jp', pub_date:'1990', created_at:'1990-08-22')
+    article = R2S::Article.new(id:1,
+                               title:'hoge',
+                               body:'<p>hoge</p>',
+                               url:'http://yahoo.co.jp',
+                               pub_date:'2017-11-03 16:20:15',
+                               created_at:'1990-08-22')
     expect(article.id).to eq 1
     expect(article.title).to eq 'hoge'
     expect(article.body).to eq '<p>hoge</p>'
     expect(article.url).to eq 'http://yahoo.co.jp'
-    expect(article.pub_date).to eq '1990'
+    expect(article.pub_date).to eq '2017-11-03 16:20:15'
     expect(article.created_at).to eq '1990-08-22'
   end
 
@@ -29,5 +33,16 @@ describe R2S::Article do
     expect(article.url).to eq nil
     expect(article.pub_date).to eq nil
     expect(article.created_at).to eq nil
+  end
+
+  it 'convert pub date' do
+    article = R2S::Article.new(pub_date:'2017-11-03 16:20:15 +900')
+    expect(article.pub_date).to eq '2017-11-03 16:20:15'
+
+    article = R2S::Article.new(pub_date:'2017-11-03')
+    expect(article.pub_date).to eq '2017-11-03 00:00:00'
+
+    article = R2S::Article.new(pub_date:'2017')
+    expect(article.pub_date).to eq nil
   end
 end
