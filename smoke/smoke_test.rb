b1e836067a2fd0f02402632210dfbabbe797c3ce
naoto0822 <n.h.in.m.h@gmail.com>
2017-11-26 19:20:45 +0900

    tmp commit

--- smoke/smoke_test.rb
+++ smoke/smoke_test.rb
@@ -4,6 +4,8 @@ require 'uri'
 require 'json'
 require 'yaml'
 require 'optparse'
+require 'openssl'
+require 'logger'
 
 # constants
 TARGET_DOMAIN = 'staging.rss2slack.co.jp'.freeze
@@ -15,8 +17,8 @@ CERT_FILE_PATH = '../private/rss2slack/staging_rss2slack.crt'.freeze
 def secret_var(path:nil)
   path = SECRET_VAR_FILE if path.nil?
   YAML.load_file(path)
-rescue
-  exit 1
+rescue => e
+  e
 end
 
 def make_body(vars, text)
@@ -50,57 +52,58 @@ end
 
 def request(uri, req)
   res = Net::HTTP.start(uri.host, uri.port,
-                      use_ssl: uri.scheme == 'https') { |http|
+                        use_ssl: uri.scheme == 'https') { |http|
     http.open_timeout = 5
     http.read_timeout = 5
-    # TODO: ssl
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    http.ca_file = CERT_FILE_PATH
     http.request(req)
   }
+  res
+rescue => e
+  e
+end
+
+def fail(e, logger)
+  logger.fail(["#{e.class}", "#{e.message}"].join(': '))
+  exit 1
+end
+
+def output_res(res, logger)
+  logger.debug(':Response')
+  logger.debug(res)
+  logger.debug(':Status Code')
+  logger.debug(res.code)
+  logger.debug(':Header')
+  res.each { |k, v| logger.debug("#{k} => #{v}") }
+  logger.debug(':Body')
+  logger.debug(res.body)
 end
 
 # valid text
 # "rss2slack_register, feed_name, url"
+def work
+  args = ARGV.getopts('t:')
 
-secret_var = YAML.load_file("../private/rss2slack/external_vars.yml")
-url = 'http://192.168.56.40/v1/slack/feed'
+  if args['t'].nil?
+    puts 'Usage: ruby slack_test_client -t <text>'
+    exit 1
+  end
 
-args = ARGV.getopts('t:')
+  text = args['t']
 
-if args['t'].nil?
-  puts 'Usage: ruby slack_test_client -t <text>'
-  exit 1
+  logger = Logger.new(STDOUT)
+  base_url = "https://#{TARGET_DOMAIN}"
+
+  # status
+  status_url = url + "/status"
+  res = get(status_url)
+  output_res(res, logger)
+
+  # slack/feed
+
+
 end
 
-text = args['t']
+work
 
-body = {
-  'token' => secret_var["dev_outgoing_webhooks_token"],
-  'team_id' => 'Personal',
-  'team_domain' => secret_var["dev_accept_team_domain"],
-  'channel_id' => secret_var["dev_accept_channel_id"],
-  'channel_name' => 'dev-rss2slack',
-  'timestamp' => 'timestamp_hoge',
-  'user_id' => 'user_id_hoge',
-  'user_name' => 'naoto0822',
-  'text' => "#{text}",
-  'trigger_word' => 'rss2slack_register'
-}
-
-uri = URI.parse(url)
-req = Net::HTTP::Post.new(uri.path)
-req.set_form_data(body)
-res = Net::HTTP.start(uri.host, uri.port,
-                      use_ssl: uri.scheme == 'https') { |http|
-  http.open_timeout = 5
-  http.read_timeout = 5
-  http.request(req)
-}
-
-p ':Response'
-p res
-p ':Status Code'
-p res.code
-p ':Header'
-res.each { |k, v| p "#{k} => #{v}" }
-p ':Body'
-p res.body
