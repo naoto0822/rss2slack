4fc479cc075ee6992737e4639e5777afb132e696
naoto0822 <n.h.in.m.h@gmail.com>
2017-12-06 21:17:18 +0900

    fix certbot option

--- playbook/roles/nginx/files/override_cert.sh
+++ playbook/roles/nginx/files/override_cert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # override launch mode to webroot
-/usr/local/certbot/certbot-auto certonly --webroot -w /var/www/hogehoge -d rss2slack.com --agree-tos --force-renewal -n
+/usr/local/certbot/certbot-auto certonly --webroot -w /var/ssl -d rss2slack.com --agree-tos --force-renewal -n
