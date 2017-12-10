62fed2db9588d54ed15d5341d850524b9b329cef
naoto0822 <n.h.in.m.h@gmail.com>
2017-12-10 15:38:18 +0900

    change logger

--- src/exec/article_worker.rb
+++ src/exec/article_worker.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
-require 'logger'
+require_relative '../app/logger'
 require_relative '../app/conf'
 require_relative '../app/article_runner'
 
 conf = R2S::Conf.new
-logger = Logger.new(conf.logger_runner_path)
+logger = R2S::Logger.new(conf.logger_runner_path)
 logger.level = Logger::DEBUG
 
 runner = R2S::ArticleRunner.new(logger, conf)
