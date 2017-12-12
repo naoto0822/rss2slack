9f06fa7fcabb23c2abf1fdd5357cae2f11df8eed
naoto0822 <n.h.in.m.h@gmail.com>
2017-12-12 22:43:33 +0900

    update manual sql

--- scripts/create_db_and_user.sql
+++ scripts/create_db_and_user.sql
@@ -1,5 +1,7 @@
 -- setup user
 CREATE DATABASE IF NOT EXISTS `rss2slack` CHARACTER SET utf8;
 CREATE USER 'rss2slack'@'localhost' IDENTIFIED BY 'Rss2SlackRss2Slack';
-GRANT ALL PRIVILEGES ON rss2slack.* TO 'rss2slack'@'localhost';
+GRANT ALL ON rss2slack.* TO 'rss2slack'@'localhost';
+CREATE USER 'rss2slack_reload'@'localhost' IDENTIFIED BY 'Rss2SlackRss2Slack';
+GRANT RELOAD ON *.* TO 'rss2slack_reload'@'localhost';
 FLUSH PRIVILEGES;
